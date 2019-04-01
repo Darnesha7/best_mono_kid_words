6af994a460393950dac099d2337b0e3773ae8f7b
Darnesha Collier <dcollier@xula.edu>
2019-04-01 17:16:44 -0500

    merged in the SUBTLEXusfrequencyabove1

--- mono_freq_aoa_score_sort.py
+++ mono_freq_aoa_score_sort.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import matplotlib as plt
+import numpy as np
 
 def rescore(d,weight_on_freq):
     wf = weight_on_freq
@@ -14,8 +15,11 @@ mono_partial['freq_lcnl_log_flipscale'].corr(mono_partial['score'])
 mono_partial['aoa_mean_scale'].corr(mono_partial['score'])
 
 mono_scored = mono_partial[mono_partial.score.notnull()].sort_values(by ='score', ascending = True)
-mono_scored.to_csv('mono_scored.csv', na_rep = '#N/A')
+#mono_scored.to_csv('mono_scored.csv', na_rep = '#N/A')
 ms = mono_scored[mono_scored["freq_lcnl_log"] > 0]
 print (ms)
 min(ms["freq_lcnl_log"])
-
+sl = pd.read_excel("word-level_data/SUBTLEXusfrequencyabove1.xlsx")
+mono_scored_merge = mono_scored.merge(sl, on = 'word', how = 'left')
+mono_scored_merge['log_value'] = np.log(mono_scored_merge['FREQcount'])
+print(mono_scored_merge)
