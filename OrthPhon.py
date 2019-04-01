0b4a7172fdbe0c5f85bb137304b96803bdc5198d
Darnesha <darnesha.collier@gmai.com>
2019-04-01 14:38:22 -0500

    We added the score, generated histogram, and added frequency.

---
+++ OrthPhon.py
@@ -0,0 +1,36 @@
+import pandas as pd
+words= pd.read_csv('3k/words.csv', header=None, keep_default_na = False)
+phon= pd.read_csv('3k/phon.csv', header=None)
+orth= pd.read_csv('3k/orth.csv', header=None)
+#words[words[0].isnull()] = 'null'
+
+phon = phon.set_index(words[0])
+orth = orth.set_index(words[0])
+
+phon.loc[['fawn','hitch']]
+
+mono = pd.read_csv ('mono_partial1000.csv')
+print(mono.shape)
+mono = mono[mono['source'] == '3k']
+print(mono.shape)
+aoa = mono['word'][0:300]
+#print (aoa)
+
+x = [0]*2881
+y = [0]*2881
+for i in range(2881):
+    x[i] = any(aoa.eq(words[0][i]))
+    y[i] = not x[i]
+    
+sum(x)
+
+#opwords = pd.read_csv("3k/opwords.csv")
+#training = opwords[opwords["value"] == "training"]
+#training_set = training[["words"]]
+training_set = pd.Series(words[x][0])
+testing_set = pd.Series(words[y][0])
+
+orth.loc[training_set].to_csv("3k/orth_aoa300_reps_train.csv", header=False)
+orth.loc[testing_set].to_csv("3k/orth_aoa300_reps_test.csv", header=False)
+phon.loc[training_set].to_csv("3k/phon_aoa300_reps_train.csv", header=False)
+phon.loc[testing_set].to_csv("3k/phon_aoa300_reps_test.csv", header=False)
---
+++ mono_freq_aoa_score_sort.py
@@ -0,0 +1,21 @@
+import pandas as pd
+import matplotlib as plt
+
+def rescore(d,weight_on_freq):
+    wf = weight_on_freq
+    wa = 1 #weight on aoa
+    x = d['freq_lcnl_log_flipscale'].multiply(weight_on_freq)
+    return d['aoa_mean_scale'] + x
+
+mono_partial= pd.read_csv('mono_partial.csv')
+
+mono_partial['score'] = rescore(mono_partial,0.5)
+mono_partial['freq_lcnl_log_flipscale'].corr(mono_partial['score'])
+mono_partial['aoa_mean_scale'].corr(mono_partial['score'])
+
+mono_scored = mono_partial[mono_partial.score.notnull()].sort_values(by ='score', ascending = True)
+mono_scored.to_csv('mono_scored.csv', na_rep = '#N/A')
+ms = mono_scored[mono_scored["freq_lcnl_log"] > 0]
+print (ms)
+min(ms["freq_lcnl_log"])
+
