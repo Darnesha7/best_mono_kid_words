e552df2fa01ef85d158ef4d9bbb9cdfaba9504bb
ReLearn <chriscox@lsu.edu>
2019-04-03 14:07:55 -0700

    regenerating log transformation

--- mono_freq_aoa_score_sort.py
+++ mono_freq_aoa_score_sort.py
@@ -1,25 +1,39 @@
 import pandas as pd
 import matplotlib as plt
 import numpy as np
+import math
 
-def rescore(d,weight_on_freq):
+def rescore(weight_on_freq):
     wf = weight_on_freq
     wa = 1 #weight on aoa
-    x = d['freq_lcnl_log_flipscale'].multiply(weight_on_freq)
+    x = ['freq_lcnl_log_flipscale'].multiply(weight_on_freq)
     return d['aoa_mean_scale'] + x
 
-mono_partial= pd.read_csv('mono_partial.csv')
+def flip_values(x):
+    flip_values = max(x)-x
+    return flipped
 
+def scale_between(x, minval, maxval):
+    y = x- min(x)
+    z = y.div(max(x))
+    m = z.multiply((max(x))-([min(x)])
+    a = m + min(x)
+    return scaled
+
+def log_transform(x):
+    freq_lcnl_log = math.log10(freq_lcnl)
+    log_trans = log(x + 1)
+    return log_trans
+mono_partial = pd.read_csv('mono_partial.csv')
+
+xlrd >= 1.0
 mono_partial['score'] = rescore(mono_partial,0.5)
 mono_partial['freq_lcnl_log_flipscale'].corr(mono_partial['score'])
 mono_partial['aoa_mean_scale'].corr(mono_partial['score'])
 
 mono_scored = mono_partial[mono_partial.score.notnull()].sort_values(by ='score', ascending = True)
-#mono_scored.to_csv('mono_scored.csv', na_rep = '#N/A')
-ms = mono_scored[mono_scored["freq_lcnl_log"] > 0]
-print (ms)
-min(ms["freq_lcnl_log"])
+new_mono_scored.to_csv('new_mono_scored.csv', na_rep = '#N/A') 
 sl = pd.read_excel("word-level_data/SUBTLEXusfrequencyabove1.xlsx")
-mono_scored_merge = mono_scored.merge(sl, on = 'word', how = 'left')
-mono_scored_merge['log_value'] = np.log(mono_scored_merge['FREQcount'])
-print(mono_scored_merge)
+new_ms= m.merge(sl, on = 'word', how = 'left')
+new_ms_merge['log_value'] = np.log(new_ms_merge['FREQcount'])
+print(new_ms_merge)
